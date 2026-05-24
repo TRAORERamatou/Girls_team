b0c1c4d15a13f3b56cf8f0ceeb7530f6499de7a3
Grace Oceane <kaboregraceoceane@gmail.com>
2026-05-24 21:32:30 +0000

    Add Car and LuxuryCar classes in vehicle.py

---
+++ vehicle.py
@@ -0,0 +1,33 @@
+# vehicle.py
+
+class Vehicle:
+    def __init__(self, make, model, year, daily_rate):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.daily_rate = daily_rate
+        self.available = True
+
+    def __str__(self):
+        return f"{self.year} {self.make} {self.model} - {self.daily_rate:,.0f} FCFA/day"
+
+
+class Car(Vehicle):
+    def __init__(self, make, model, year, daily_rate, num_doors, car_type):
+        super().__init__(make, model, year, daily_rate)
+        self.num_doors = num_doors
+        self.car_type = car_type
+
+    def __str__(self):
+        return f"{super().__str__()} ({self.car_type}, {self.num_doors} doors)"
+
+
+class LuxuryCar(Car):
+    def __init__(self, make, model, year, daily_rate, num_doors, car_type, premium_features, chauffeur_available):
+        super().__init__(make, model, year, daily_rate, num_doors, car_type)
+        self.premium_features = premium_features
+        self.chauffeur_available = chauffeur_available
+
+    def __str__(self):
+        chauffeur = "with chauffeur" if self.chauffeur_available else "no chauffeur"
+        return f"{super().__str__()} [Luxury: {self.premium_features}, {chauffeur}]"
