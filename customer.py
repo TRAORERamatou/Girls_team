f874f5899d91f82712f14f2711c136a2be95d3af
Aminata <aminaoue4002@gmail.com>
2026-05-24 21:55:13 +0000

    mon premier commit

--- customer.py
+++
@@ -1,9 +0,0 @@
-# customer.py
-# Project: Car Rental — PRG1406
-
-class Customer:
-    def __init__(self, nom, age, permis, is_member):
-        self.nom = nom              # str
-        self.age = age              # int
-        self.permis = permis        # str
-        self.is_member = is_member  # bool
