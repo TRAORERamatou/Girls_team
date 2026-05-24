ba58be563c0c44a52bb870e928597678a3942c9b
TRAORERamatou <ramatraore209@gmail.com>
2026-05-24 22:34:11 +0000

    yes

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from customer import Customer
 from rental import Rental
 
 
-# ── Validated Input Functionsr ─────────────────────────────────
+# ── Validated Input Functions ─────────────────────────────────
 
 def get_text_input(message):
     """Prompts for a non-empty string."""
